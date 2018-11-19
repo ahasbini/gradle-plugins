6081d0e29bdec1d727db61de6346bb0c12f33cf1
ahasbini <a.g.hasbini@gmail.com>
2018-11-19 11:20:32 +0200

    Minor fixes for package name and  plugins config block in build.gradle

--- plugins/src/main/groovy/com/inutilfutil/gradle/android/ResourcePlaceholdersPlugin.groovy
+++ plugins/src/main/groovy/com/inutilfutil/gradle/android/ResourcePlaceholdersPlugin.groovy
@@ -4,6 +4,7 @@ import org.gradle.api.Project
 import org.gradle.api.Plugin
 
 class ResourcePlaceholdersPlugin implements Plugin<Project> {
+
     void apply(Project project) {
         project.afterEvaluate {
             //Recursively apply to subprojects
